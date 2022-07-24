8e109d3defc22136d6b9404dd3cd3c72025fe37b
thulanimbatha <thulanimbatha06@gmail.com>
2022-07-24 21:37:33 +0200

    created interface

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flash Card Project")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+canvas = Canvas(width=900, height=550, bg=BACKGROUND_COLOR, highlightthickness=0)
+flash_card = PhotoImage(file="./images/card_front.png")
+canvas.create_image(450, 270, image=flash_card)
+canvas.grid(column=0, row=0, columnspan=2)
+
+wrong_img = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+right_img = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_img, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+window.mainloop()
